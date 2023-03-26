9ece335e4b7c36b3afd5b27a06e8495b29700c6d
Max Kruggel <support@redigermany.de>
2023-03-26 17:25:59 +0200

    added _str_ methods

--- APIResponse.py
+++ APIResponse.py
@@ -1,10 +1,16 @@
 from typing import List
 
+import jsonpickle
+
 class Position:
     def __init__(self,x:int,y:int) -> None:
         self.x = x
         self.y = y
         pass
+    def toTuple(self):
+        return (self.x,self.y)
+    def __str__(self) -> str:
+        return jsonpickle.encode(self, unpicklable=False)
 
 class Door:
     def __init__(self,fromPosition:int,toPosition:int,hinge:int,openLeft:bool,style:str) -> None:
@@ -14,6 +20,8 @@ class Door:
         self.openLeft = openLeft
         self.style = style
         pass
+    def __str__(self) -> str:
+        return jsonpickle.encode(self, unpicklable=False)
 
 class Window:
     def __init__(self,fromPosition:int,toPosition:int,style:str) -> None:
@@ -21,6 +29,8 @@ class Window:
         self.toPosition = toPosition
         self.style = style
         pass
+    def __str__(self) -> str:
+        return jsonpickle.encode(self, unpicklable=False)
 
 class Wall:
     def __init__(self,fromPosition:Position,toPosition:Position,isHorizontal:bool,isOuterWall:bool,doors:List[Door],windows:List[Window]) -> None:
@@ -31,11 +41,15 @@ class Wall:
         self.doors = doors
         self.windows = windows
         pass
+    def __str__(self) -> str:
+        return jsonpickle.encode(self, unpicklable=False)
 
 class Junction:
     def __init__(self,success:bool,walls:None) -> None:
         self.success = success
         pass
+    def __str__(self) -> str:
+        return jsonpickle.encode(self, unpicklable=False)
 
 class APIResponse:
     def __init__(self,success:bool,walls:List[Wall],junctions:List[Junction]) -> None:
@@ -43,3 +57,5 @@ class APIResponse:
         self.walls = walls
         self.junctions = junctions
         pass
+    def __str__(self) -> str:
+        return jsonpickle.encode(self, unpicklable=False)
