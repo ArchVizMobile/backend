c5a5d806d5ba14d686478c6e0ec4e304640cd911
ReDiGermany <support@redigermany.de>
2023-03-31 09:50:48 +0200

    Added broken plans for debug - switching to dev branch now

--- data.py
+++ data.py
@@ -1,5 +1,7 @@
 import random
 from typing import List
+
+import jsonpickle
 from APIResponse import APIResponse, Door, Position, SimplePosition, Wall, Window
 from config import CONFIG
 from generateCorners import generateCorners
@@ -28,15 +30,20 @@ def getData():
 
     # rooms = [[{'x': 100, 'y': 100, 'generated': True}, {'x': 574, 'y': 100, 'generated': True}, {'x': 1044, 'y': 100, 'generated': True}], [{'x': 100, 'y': 325, 'generated': True}, {'x': 100, 'y': 325, 'generated': False}, {'x': 373, 'y': 325, 'generated': True}, {'x': 574, 'y': 325, 'generated': False}, {'x': 612, 'y': 325, 'generated': True}, {'x': 970, 'y': 325, 'generated': True}, {'x': 1044, 'y': 325, 'generated': False}], [{'x': 100, 'y': 795, 'generated': True}, {'x': 100, 'y': 795, 'generated': False}, {'x': 373, 'y': 795, 'generated': False}, {'x': 558, 'y': 795, 'generated': True}, {'x': 612, 'y': 795, 'generated': False}, {'x': 948, 'y': 795, 'generated': True}, {'x': 970, 'y': 795, 'generated': False}, {'x': 1210, 'y': 795, 'generated': True}, {'x': 1496, 'y': 795, 'generated': True}], [{'x': 100, 'y': 995, 'generated': False}, {'x': 558, 'y': 995, 'generated': False}, {'x': 948, 'y': 995, 'generated': False}, {'x': 1210, 'y': 995, 'generated': False}, {'x': 1496, 'y': 995, 'generated': False}]]
     # rooms = [[{'x': 100, 'y': 100, 'generated': True}, {'x': 413, 'y': 100, 'generated': True}, {'x': 650, 'y': 100, 'generated': True}, {'x': 1028, 'y': 100, 'generated': True}], [{'x': 100, 'y': 441, 'generated': True}, {'x': 100, 'y': 441, 'generated': False}, {'x': 413, 'y': 441, 'generated': False}, {'x': 441, 'y': 441, 'generated': True}, {'x': 650, 'y': 441, 'generated': False}, {'x': 905, 'y': 441, 'generated': True}, {'x': 1028, 'y': 441, 'generated': False}, {'x': 1152, 'y': 441, 'generated': True}], [{'x': 100, 'y': 724, 'generated': True}, {'x': 100, 'y': 724, 'generated': False}, {'x': 353, 'y': 724, 'generated': True}, {'x': 441, 'y': 724, 'generated': False}, {'x': 737, 'y': 724, 'generated': True}, {'x': 905, 'y': 724, 'generated': False}, {'x': 1013, 'y': 724, 'generated': True}, {'x': 1152, 'y': 724, 'generated': False}], [{'x': 100, 'y': 1088, 'generated': True}, {'x': 100, 'y': 1088, 'generated': False}, {'x': 353, 'y': 1088, 'generated': False}, {'x': 534, 'y': 1088, 'generated': True}, {'x': 737, 'y': 1088, 'generated': False}, {'x': 810, 'y': 1088, 'generated': True}, {'x': 1013, 'y': 1088, 'generated': False}, {'x': 1302, 'y': 1088, 'generated': True}], [{'x': 100, 'y': 1303, 'generated': True}, {'x': 100, 'y': 1303, 'generated': False}, {'x': 413, 'y': 1303, 'generated': True}, {'x': 534, 'y': 1303, 'generated': False}, {'x': 656, 'y': 1303, 'generated': True}, {'x': 810, 'y': 1303, 'generated': False}, {'x': 1302, 'y': 1303, 'generated': False}], [{'x': 100, 'y': 1503, 'generated': False}, {'x': 413, 'y': 1503, 'generated': False}, {'x': 656, 'y': 1503, 'generated': False}]]
+    
+    # Aktuell Kaputt:
+    # rooms = [[{'x': 100, 'y': 100, 'generated': True}, {'x': 333, 'y': 100, 'generated': True}, {'x': 731, 'y': 100, 'generated': True}, {'x': 1126, 'y': 100, 'generated': True}, {'x': 1370, 'y': 100, 'generated': True}, {'x': 1730, 'y': 100, 'generated': True}], [{'x': 100, 'y': 477, 'generated': True}, {'x': 100, 'y': 477, 'generated': False}, {'x': 333, 'y': 477, 'generated': False}, {'x': 339, 'y': 477, 'generated': True}, {'x': 731, 'y': 477, 'generated': False}, {'x': 837, 'y': 477, 'generated': True}, {'x': 1126, 'y': 477, 'generated': False}, {'x': 1370, 'y': 477, 'generated': False}, {'x': 1730, 'y': 477, 'generated': False}], [{'x': 100, 'y': 892, 'generated': True}, {'x': 100, 'y': 892, 'generated': False}, {'x': 339, 'y': 892, 'generated': False}, {'x': 347, 'y': 892, 'generated': True}, {'x': 567, 'y': 892, 'generated': True}, {'x': 837, 'y': 892, 'generated': False}, {'x': 918, 'y': 892, 'generated': True}, {'x': 1370, 'y': 892, 'generated': True}], [{'x': 100, 'y': 1324, 'generated': True}, {'x': 100, 'y': 1324, 'generated': False}, {'x': 347, 'y': 1324, 'generated': False}, {'x': 548, 'y': 1324, 'generated': True}, {'x': 567, 'y': 1324, 'generated': False}, {'x': 918, 'y': 1324, 'generated': False}, {'x': 936, 'y': 1324, 'generated': True}, {'x': 1370, 'y': 1324, 'generated': False}], [{'x': 100, 'y': 1575, 'generated': True}, {'x': 100, 'y': 1575, 'generated': False}, {'x': 488, 'y': 1575, 'generated': True}, {'x': 548, 'y': 1575, 'generated': False}, {'x': 751, 'y': 1575, 'generated': True}, {'x': 936, 'y': 1575, 'generated': False}, {'x': 1191, 'y': 1575, 'generated': True}], [{'x': 100, 'y': 1775, 'generated': False}, {'x': 488, 'y': 1775, 'generated': False}, {'x': 751, 'y': 1775, 'generated': False}, {'x': 1191, 'y': 1775, 'generated': False}]]
+    rooms = [[{'x': 100, 'y': 100, 'generated': True}, {'x': 458, 'y': 100, 'generated': True}, {'x': 686, 'y': 100, 'generated': True}, {'x': 1137, 'y': 100, 'generated': True}, {'x': 1343, 'y': 100, 'generated': True}, {'x': 1572, 'y': 100, 'generated': True}], [{'x': 100, 'y': 412, 'generated': True}, {'x': 100, 'y': 412, 'generated': False}, {'x': 378, 'y': 412, 'generated': True}, {'x': 458, 'y': 412, 'generated': False}, {'x': 686, 'y': 412, 'generated': False}, {'x': 747, 'y': 412, 'generated': True}, {'x': 960, 'y': 412, 'generated': True}, {'x': 1137, 'y': 412, 'generated': False}, {'x': 1343, 'y': 412, 'generated': False}, {'x': 1404, 'y': 412, 'generated': True}, {'x': 1572, 'y': 412, 'generated': False}], [{'x': 100, 'y': 723, 'generated': True}, {'x': 100, 'y': 723, 'generated': False}, {'x': 322, 'y': 723, 'generated': True}, {'x': 378, 'y': 723, 'generated': False}, {'x': 568, 'y': 723, 'generated': True}, {'x': 747, 'y': 723, 'generated': False}, {'x': 886, 'y': 723, 'generated': True}, {'x': 960, 'y': 723, 'generated': False}, {'x': 1404, 'y': 723, 'generated': False}], [{'x': 100, 'y': 1170, 'generated': True}, {'x': 100, 'y': 1170, 'generated': False}, {'x': 306, 'y': 1170, 'generated': True}, {'x': 322, 'y': 1170, 'generated': False}, {'x': 559, 'y': 1170, 'generated': True}, {'x': 568, 'y': 1170, 'generated': False}, {'x': 886, 'y': 1170, 'generated': False}, {'x': 969, 'y': 1170, 'generated': True}, {'x': 1299, 'y': 1170, 'generated': True}], [{'x': 100, 'y': 1370, 'generated': False}, {'x': 306, 'y': 1370, 'generated': False}, {'x': 559, 'y': 1370, 'generated': False}, {'x': 969, 'y': 1370, 'generated': False}, {'x': 1299, 'y': 1370, 'generated': False}]]  
     print(rooms)
 
     wall_width = CONFIG.getWALL_WIDTH()
     wall_width_half = wall_width/2
 
-    # for idx,room in enumerate(rooms):
-    #     for idx1,room1 in enumerate(room):
-    #         room1["x"] = room1["x"] - (room1["x"] % 20)
-    #         room1["y"] = room1["y"] - (room1["y"] % 20)
+    # TODO: When grid enabled: fix joint jumping
+    for idx,room in enumerate(rooms):
+        for idx1,room1 in enumerate(room):
+            room1["x"] = room1["x"] - (room1["x"] % 20)
+            room1["y"] = room1["y"] - (room1["y"] % 20)
 
     for idx,room in enumerate(rooms):
         for idx1,room1 in enumerate(room):
@@ -65,7 +72,7 @@ def getData():
                     "bottom": False,
                     "right": False
                 }})
-    
+            
     for idx,room in enumerate(joints):
         for idx1,room1 in enumerate(joints):
             # if idx!=idx1:
