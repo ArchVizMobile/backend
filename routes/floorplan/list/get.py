76a6b24e02b7d6865ed74869cccd151532d83b14
ReDiGermany <support@redigermany.de>
2023-04-24 09:32:47 +0200

    changed to absolute links

--- routes/floorplan/list/get.py
+++ routes/floorplan/list/get.py
@@ -1,11 +1,13 @@
 from bson import ObjectId
 
+from config import CONFIG
+
 def GET(self,dbCollection,search):
     lst = []
     for item in dbCollection.find():
         lst.append({
             "id": str(item.get('_id')),
             "name": item.get("name"),
-            "image": "/image/"+str(item.get('_id'))
+            "image": f"{CONFIG.getWEB_PROTOCOL()}://{CONFIG.getWEB_HOST()}:{CONFIG.getWEB_PORT()}/image?id="+str(item.get('_id'))
         })
     return {"success":True,"list":lst}
