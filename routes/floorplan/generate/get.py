9b7647ba0d0b9c6ecc3a85ea439a0251095b8ce0
ReDiGermany <support@redigermany.de>
2023-04-24 09:32:20 +0200

    Fixed walls/doors => features

--- routes/floorplan/generate/get.py
+++ routes/floorplan/generate/get.py
@@ -17,18 +17,12 @@ def GET(self,dbCollection,search):
             w.fromPosition.y = int(w.fromPosition.y * s)
             w.toPosition.x = int(w.toPosition.x * s)
             w.toPosition.y = int(w.toPosition.y * s)
-            for item in w.doors:
+            for item in w.features:
                 item.fromPosition = int(item.fromPosition * s)
                 item.toPosition = int(item.toPosition * s)
                 item.z = int(item.z * s)
                 item.height = int(item.height * s)
                 item.hinge = int(item.hinge * s)
-
-            for item in w.windows:
-                item.fromPosition = int(item.fromPosition * s)
-                item.toPosition = int(item.toPosition * s)
-                item.z = int(item.z * s)
-                item.height = int(item.height * s)
 
         for r in rooms:
             r.fromPosition.x = int(r.fromPosition.x * s)
@@ -37,7 +31,7 @@ def GET(self,dbCollection,search):
             r.toPosition.y = int(r.toPosition.y * s)
 
         #wallsobj,junctions,wallsarr = getData()
-        draw(junctions,walls,rooms)
+        # draw(junctions,walls,rooms)
     except Exception as e:
         print(e)
         walls,junctions,rooms = "no","no","no"
