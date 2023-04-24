f393507d5998e30bba19075852a3598f453786e3
ReDiGermany <support@redigermany.de>
2023-04-24 07:35:30 +0200

    Implemented mongodb and dynamic route loading

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
+from datetime import date
 from http.server import BaseHTTPRequestHandler,HTTPServer
+import os
+import re
+from bson import ObjectId
 import jsonpickle
 from APIResponse import Wall
 import typing
@@ -6,6 +10,23 @@ import typing
 from data import getData
 from config import CONFIG
 from draw import draw
+
+import pymongo
+import random
+import string
+from datetime import datetime
+from urllib.parse import  urlparse
+
+from apiroutes import loadDynamicAPI
+API = loadDynamicAPI(debug=True,fileDebug=False)
+
+myclient = pymongo.MongoClient(f"mongodb://{CONFIG.getDB_HOST()}:{CONFIG.getDB_PORT()}/")
+mydb = myclient[CONFIG.getDB_DATABASE()]
+mycollection = mydb["floorplans"]
+
+def get_random_string(length):
+    result_str = ''.join(random.choice(string.ascii_letters) for i in range(length))
+    return result_str
 
 class Server(BaseHTTPRequestHandler):
 
@@ -16,6 +37,15 @@ class Server(BaseHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Methods', 'GET')
         self.send_header("Access-Control-Allow-Headers", "X-Requested-With, Content-type")
 
+        path = self.path
+
+        if path in API["POST"]:
+            response = API["POST"][path](path)
+            json = jsonpickle.encode(response, unpicklable=False)
+
+            self.wfile.write(json.encode())
+            return
+
     # GET sends back a Hello world message
     def do_GET(self):
         self.send_response(200)
@@ -25,6 +55,60 @@ class Server(BaseHTTPRequestHandler):
         self.send_header("Access-Control-Allow-Headers", "X-Requested-With, Content-type")
         #self.send_header('Content-type','text/html')
         self.end_headers()
+
+        path = self.path.split("?")[0]
+        # params = self.path.split("?")[1]
+        url = urlparse(self.path)
+        search = {}
+        if url.query !="":
+            for item in url.query.split("&"):
+                key,value = item.split("=")
+                search[key]=value
+        # print(search)
+
+        if path in API["GET"]:
+            self.send_header('Content-type','application/json')
+            # self.
+            response = API["GET"][path](self,search=search,dbCollection=mycollection)
+            json = jsonpickle.encode(response, unpicklable=False)
+
+            self.wfile.write(json.encode())
+            return
+
+        if self.path.startswith("/get"):
+            self.send_header('Content-type','application/json')
+            search = {"_id":ObjectId(self.path.split("/")[2])}
+            cnt = mycollection.count_documents(search)
+            ret = {
+                "success": False,
+                "walls": [],
+                "junctions": [],
+                "rooms": [],
+                "scale": -1,
+            }
+
+            if cnt > 0:
+                lst = mycollection.find(search)
+                ret = lst[0]
+                ret["_id"] = str(ret["_id"])
+
+            json = jsonpickle.encode(ret, unpicklable=False)
+
+            self.wfile.write(json.encode())
+            return
+        # if self.path.endswith("/list"):
+        #     self.send_header('Content-type','application/json')
+        #     lst = []
+        #     for item in mycollection.find():
+        #         lst.append({
+        #             "id": str(item.get('_id')),
+        #             "name": item.get("name"),
+        #             "image": "/image/"+str(item.get('_id'))
+        #         })
+        #         # lst.append((item.get('_id')))
+        #     json = jsonpickle.encode({"success":True,"list":lst}, unpicklable=False)
+        #     self.wfile.write(json.encode())
+        #     return
         if self.path.endswith("/"):
             self.send_header('Content-type','application/json')
             s = 1.5
@@ -63,13 +147,21 @@ class Server(BaseHTTPRequestHandler):
             except Exception as e:
                 print(e)
                 walls,junctions,rooms = "no","no","no"
-            self.wfile.write(jsonpickle.encode({
+            now = datetime.now()
+            dt_string = now.strftime("%d.%m.%Y %H:%M:%S")
+
+            data = {
                 "success": walls!="no",
+                "name": f"Generated Floorplan from {dt_string}",
                 "walls": walls,
                 "junctions": junctions,
                 "rooms": rooms,
-                "scale": s
-            }, unpicklable=False).encode())
+                "scale": s,
+            }
+            json = jsonpickle.encode(data, unpicklable=False)
+            if data["success"]:
+                x = mycollection.insert_one(jsonpickle.decode(json))
+            self.wfile.write(json.encode())
             return
         if self.path.endswith("/dash.html"):
             self.send_header('Content-type','text/html')
