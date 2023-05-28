4d05f67323812cd9203bc854f2fb1d6d1cff1a09
Max Kruggel <support@redigermany.de>
2023-05-28 13:23:13 +0200

    Added info

--- routes/floorplan/list/get.py
+++ routes/floorplan/list/get.py
@@ -8,6 +8,14 @@ def GET(self,dbCollection,search):
         lst.append({
             "id": str(item.get('_id')),
             "name": item.get("name"),
-            "image": f"{CONFIG.getWEB_PROTOCOL()}://{CONFIG.getWEB_HOST()}:{CONFIG.getWEB_PORT()}/image?id="+str(item.get('_id'))
+            "image": f"{CONFIG.getWEB_PROTOCOL()}://{CONFIG.getWEB_HOST()}:{CONFIG.getWEB_PORT()}/image?id="+str(item.get('_id')),
+            "info":{
+                "number_of_rooms": 1337,
+                "qm": 42,
+                "floors": 69,
+                "vendor": "self",
+                "background": "city",
+                "facts": "lelele rofl keepo",
+            }
         })
     return {"success":True,"list":lst}
