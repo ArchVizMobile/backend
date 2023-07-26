692a23cf984f5f1cb3b1f03fe7ac19352996ad1a
Leander Steiner <leandersteiner@yahoo.de>
2023-07-26 16:11:33 +0200

    updated trained model

--- detection_pipeline_test.py
+++ detection_pipeline_test.py
@@ -14,13 +14,14 @@ logging.basicConfig(
 )
 
 trainer = ModelTrainer()
-# stair_model = trainer.train(Models.STAIRS, "ml/stairs/di.v2i.yolov8", 100)
+#stair_model = trainer.train(Models.STAIRS, "ml/stairs/di.v3i.yolov8", 100)
 stair_model = trainer.load(Models.STAIRS)
 
 stair_service = StairService(stair_model)
 
 detection_pipeline = PipelineBuilder().with_step(stair_service).with_step(stair_service).build()
 
-result = detection_pipeline.execute("ml/stairs/data/csm_grundriss-kern-haus-aura-einliegerwohnung-dachgeschoss_9b507b4138_jpg.rf.5a6c9493bcf127b5d900e303db9c2347.jpg")
+#result = detection_pipeline.execute("ml/stairs/data/csm_grundriss-kern-haus-aura-einliegerwohnung-dachgeschoss_9b507b4138_jpg.rf.5a6c9493bcf127b5d900e303db9c2347.jpg")
+result = detection_pipeline.execute("ml/stairs/result4.jpg")
 
 logging.info(result)
